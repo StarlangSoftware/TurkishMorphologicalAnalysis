c9cd5f0353159c7ef229d476add9e4819ab99bb8
Selen <parlarselen@gmail.com>
2018-06-13 14:32:21 +0300

    comments and examples added for documentation.

--- src/MorphologicalAnalysis/InflectionalGroup.java
+++ src/MorphologicalAnalysis/InflectionalGroup.java
@@ -3,7 +3,7 @@ package MorphologicalAnalysis;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class InflectionalGroup implements Serializable{
+public class InflectionalGroup implements Serializable {
     private ArrayList<MorphologicalTag> IG;
     public static final String[] tags = {"Noun", "Adv", "Adj", "Verb", "A1sg",
             "A2sg", "A3sg", "A1Pl", "A2pl", "A3pl",
@@ -31,12 +31,12 @@ public class InflectionalGroup implements Serializable{
             "ETTag", "BDTag", "EDTag", "Inf1", "AsLongAs",
             "Dist", "Adamantly", "Percent", "WithoutBeingAbleToHaveDoneSo", "Dim",
             "Pers", "Fraction", "Hashtag", "Email", "Date"};
-    public static final MorphologicalTag[] morphoTags = {MorphologicalTag.NOUN, MorphologicalTag.ADVERB, MorphologicalTag.ADJECTIVE, MorphologicalTag.VERB, MorphologicalTag.A1SG,
-            MorphologicalTag.A2SG, MorphologicalTag.A3SG, MorphologicalTag.A1PL, MorphologicalTag.A2PL, MorphologicalTag.A3PL,
-            MorphologicalTag.P1SG, MorphologicalTag.P2SG, MorphologicalTag.P3SG, MorphologicalTag.P1PL, MorphologicalTag.P2PL,
-            MorphologicalTag.P3PL, MorphologicalTag.PROPERNOUN, MorphologicalTag.PNON, MorphologicalTag.NOMINATIVE, MorphologicalTag.WITH,
-            MorphologicalTag.WITHOUT, MorphologicalTag.ACCUSATIVE, MorphologicalTag.DATIVE, MorphologicalTag.GENITIVE, MorphologicalTag.ABLATIVE,
-            MorphologicalTag.ZERO, MorphologicalTag.ABLE, MorphologicalTag.NEGATIVE, MorphologicalTag.PASTTENSE,
+    public static final MorphologicalTag[] morphoTags = {MorphologicalTag.NOUN, MorphologicalTag.ADVERB, MorphologicalTag.ADJECTIVE,
+            MorphologicalTag.VERB, MorphologicalTag.A1SG, MorphologicalTag.A2SG, MorphologicalTag.A3SG, MorphologicalTag.A1PL,
+            MorphologicalTag.A2PL, MorphologicalTag.A3PL, MorphologicalTag.P1SG, MorphologicalTag.P2SG, MorphologicalTag.P3SG, MorphologicalTag.P1PL,
+            MorphologicalTag.P2PL, MorphologicalTag.P3PL, MorphologicalTag.PROPERNOUN, MorphologicalTag.PNON, MorphologicalTag.NOMINATIVE,
+            MorphologicalTag.WITH, MorphologicalTag.WITHOUT, MorphologicalTag.ACCUSATIVE, MorphologicalTag.DATIVE, MorphologicalTag.GENITIVE,
+            MorphologicalTag.ABLATIVE, MorphologicalTag.ZERO, MorphologicalTag.ABLE, MorphologicalTag.NEGATIVE, MorphologicalTag.PASTTENSE,
             MorphologicalTag.CONJUNCTION, MorphologicalTag.DETERMINER, MorphologicalTag.DUPLICATION, MorphologicalTag.INTERJECTION, MorphologicalTag.NUMBER,
             MorphologicalTag.POSTPOSITION, MorphologicalTag.PUNCTUATION, MorphologicalTag.QUESTION, MorphologicalTag.AGENT, MorphologicalTag.BYDOINGSO,
             MorphologicalTag.CARDINAL, MorphologicalTag.CAUSATIVE, MorphologicalTag.DEMONSTRATIVEPRONOUN, MorphologicalTag.DISTRIBUTIVE, MorphologicalTag.FITFOR,
@@ -58,7 +58,14 @@ public class InflectionalGroup implements Serializable{
             MorphologicalTag.DISTRIBUTIVE, MorphologicalTag.ADAMANTLY, MorphologicalTag.PERCENT, MorphologicalTag.WITHOUTBEINGABLETOHAVEDONESO, MorphologicalTag.DIMENSION,
             MorphologicalTag.PERSONALPRONOUN, MorphologicalTag.FRACTION, MorphologicalTag.HASHTAG, MorphologicalTag.EMAIL, MorphologicalTag.DATE};
 
-    public static MorphologicalTag getMorphologicalTag(String tag){
+    /**
+     * The getMorphologicalTag method takes a String tag as an input and if the input matches with one of the elements of
+     * tags array, it then gets the morphoTags of this tag and returns it.
+     *
+     * @param tag String to get morphoTags from.
+     * @return morphoTags if found, null otherwise.
+     */
+    public static MorphologicalTag getMorphologicalTag(String tag) {
         for (int j = 0; j < tags.length; j++) {
             if (tag.equalsIgnoreCase(tags[j])) {
                 return morphoTags[j];
@@ -67,7 +74,13 @@ public class InflectionalGroup implements Serializable{
         return null;
     }
 
-    public static String getTag(MorphologicalTag tag){
+    /**
+     * The getTag method takes a MorphologicalTag type tag as an input and returns its corresponding tag from tags array.
+     *
+     * @param tag MorphologicalTag type input to find tag from.
+     * @return tag if found, null otherwise.
+     */
+    public static String getTag(MorphologicalTag tag) {
         for (int j = 0; j < morphoTags.length; j++) {
             if (tag.equals(morphoTags[j])) {
                 return tags[j].toUpperCase();
@@ -76,15 +89,22 @@ public class InflectionalGroup implements Serializable{
         return null;
     }
 
-    public InflectionalGroup(String IG){
+    /**
+     * A constructor of {@link InflectionalGroup} class which initializes the IG {@link ArrayList} by parsing given input
+     * String IG by + and calling the getMorphologicalTag method with these substrings. If getMorphologicalTag method returns
+     * a tag, it adds this tag to the IG {@link ArrayList}.
+     *
+     * @param IG String input.
+     */
+    public InflectionalGroup(String IG) {
         MorphologicalTag tag;
         String morphologicalTag;
         this.IG = new ArrayList<MorphologicalTag>();
         String st = IG;
-        while (st.contains("+")){
+        while (st.contains("+")) {
             morphologicalTag = st.substring(0, st.indexOf("+"));
             tag = getMorphologicalTag(morphologicalTag);
-            if (tag != null){
+            if (tag != null) {
                 this.IG.add(tag);
             } else {
                 System.out.println("Morphological Tag " + morphologicalTag + " not found\n");
@@ -93,59 +113,100 @@ public class InflectionalGroup implements Serializable{
         }
         morphologicalTag = st;
         tag = getMorphologicalTag(morphologicalTag);
-        if (tag != null){
+        if (tag != null) {
             this.IG.add(tag);
         } else {
             System.out.println("Morphological Tag " + morphologicalTag + " not found\n");
         }
     }
 
-    public MorphologicalTag getTag(int index){
+    /**
+     * Another getTag method which takes index as an input and returns the corresponding tag from IG {@link ArrayList}.
+     *
+     * @param index to get tag.
+     * @return tag at input index.
+     */
+    public MorphologicalTag getTag(int index) {
         return IG.get(index);
     }
 
-    public int size(){
+    /**
+     * The size method returns the size of the G {@link ArrayList}.
+     *
+     * @return the size of the G {@link ArrayList}.
+     */
+    public int size() {
         return IG.size();
     }
 
-    public String toString(){
+    /**
+     * Overridden toString method to return resulting tags in IG {@link ArrayList}.
+     *
+     * @return String result.
+     */
+    public String toString() {
         String result = getTag(IG.get(0));
-        for (int i = 1; i < IG.size(); i++){
-            result =  result + "+" + getTag(IG.get(i));
+        for (int i = 1; i < IG.size(); i++) {
+            result = result + "+" + getTag(IG.get(i));
         }
         return result;
     }
 
-    public MorphologicalTag containsCase(){
-        for (MorphologicalTag tag:IG){
-            if (tag.equals(MorphologicalTag.NOMINATIVE) || tag.equals(MorphologicalTag.ACCUSATIVE) || tag.equals(MorphologicalTag.DATIVE) || tag.equals(MorphologicalTag.LOCATIVE) || tag.equals(MorphologicalTag.ABLATIVE)){
+    /**
+     * The containsCase method loops through the tags in IG {@link ArrayList} and finds out the tags of the NOMINATIVE,
+     * ACCUSATIVE, DATIVE, LOCATIVE or ABLATIVE cases.
+     *
+     * @return tag which holds the condition.
+     */
+    public MorphologicalTag containsCase() {
+        for (MorphologicalTag tag : IG) {
+            if (tag.equals(MorphologicalTag.NOMINATIVE) || tag.equals(MorphologicalTag.ACCUSATIVE) || tag.equals(MorphologicalTag.DATIVE) || tag.equals(MorphologicalTag.LOCATIVE) || tag.equals(MorphologicalTag.ABLATIVE)) {
                 return tag;
             }
         }
         return null;
     }
 
-    public boolean containsPlural(){
-        for (MorphologicalTag tag:IG){
-            if (tag.equals(MorphologicalTag.A1PL) || tag.equals(MorphologicalTag.A2PL) || tag.equals(MorphologicalTag.A3PL) || tag.equals(MorphologicalTag.P1PL) || tag.equals(MorphologicalTag.P2PL) || tag.equals(MorphologicalTag.P3PL)){
+    /**
+     * The containsPlural method loops through the tags in IG {@link ArrayList} and checks whether the tags are from
+     * the agreement plural or possessive plural, i.e A1PL, A2PL, A3PL, P1PL, P2PL and P3PL.
+     *
+     * @return true if the tag is plural, false otherwise.
+     */
+    public boolean containsPlural() {
+        for (MorphologicalTag tag : IG) {
+            if (tag.equals(MorphologicalTag.A1PL) || tag.equals(MorphologicalTag.A2PL) || tag.equals(MorphologicalTag.A3PL) || tag.equals(MorphologicalTag.P1PL) || tag.equals(MorphologicalTag.P2PL) || tag.equals(MorphologicalTag.P3PL)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean containsTag(MorphologicalTag tag){
-        for (MorphologicalTag currentTag:IG){
-            if (currentTag.equals(tag)){
+    /**
+     * The containsTag method takes a MorphologicalTag type tag as an input and loops through the tags in
+     * IG {@link ArrayList} and returns true if the input matches with on of the tags in the IG.
+     *
+     * @param tag MorphologicalTag type input to search for.
+     * @return true if tag matches with the tag in IG, false otherwise.
+     */
+    public boolean containsTag(MorphologicalTag tag) {
+        for (MorphologicalTag currentTag : IG) {
+            if (currentTag.equals(tag)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean containsPossessive(){
-        for (MorphologicalTag tag:IG){
-            if (tag.equals(MorphologicalTag.P1PL) || tag.equals(MorphologicalTag.P1SG) || tag.equals(MorphologicalTag.P2PL) || tag.equals(MorphologicalTag.P2SG) || tag.equals(MorphologicalTag.P3PL) || tag.equals(MorphologicalTag.P3SG)){
+    /**
+     * The containsPossessive method loops through the tags in IG {@link ArrayList} and returns true if the tag in IG is
+     * one of the possessives: P1PL, P1SG, P2PL, P2SG, P3PL AND P3SG.
+     *
+     * @return
+     */
+    public boolean containsPossessive() {
+        for (MorphologicalTag tag : IG) {
+            if (tag.equals(MorphologicalTag.P1PL) || tag.equals(MorphologicalTag.P1SG) || tag.equals(MorphologicalTag.P2PL) || tag.equals(MorphologicalTag.P2SG) || tag.equals(MorphologicalTag.P3PL) || tag.equals(MorphologicalTag.P3SG)) {
                 return true;
             }
         }
