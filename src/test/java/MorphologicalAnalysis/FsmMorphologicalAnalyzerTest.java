2a774b86a7414f4dd0336debbbf53809e6ca3b5d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-09-08 08:02:03 +0300

    Added test cases for generateAllParses.

--- src/test/java/MorphologicalAnalysis/FsmMorphologicalAnalyzerTest.java
+++ src/test/java/MorphologicalAnalysis/FsmMorphologicalAnalyzerTest.java
@@ -52,9 +52,9 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisProperNoun() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isProperNoun()){
+            if (word.isProperNoun()) {
                 assertTrue(fsm.morphologicalAnalysis(word.getName().toUpperCase(new Locale("tr"))).size() != 0);
             }
         }
@@ -63,12 +63,12 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisNounSoftenDuringSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.nounSoftenDuringSuffixation()){
+            if (word.isNominal() && word.nounSoftenDuringSuffixation()) {
                 State transitionState = new State("Possessive", false, false);
                 State startState = new State("NominalRoot", true, false);
-                Transition transition = new Transition(transitionState,"yH", "ACC");
+                Transition transition = new Transition(transitionState, "yH", "ACC");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -76,14 +76,14 @@ public class FsmMorphologicalAnalyzerTest {
     }
 
     @Test
-    public void morphologicalAnalysisVowelAChangesToIDuringYSuffixation(){
+    public void morphologicalAnalysisVowelAChangesToIDuringYSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isVerb() && word.vowelAChangesToIDuringYSuffixation()){
+            if (word.isVerb() && word.vowelAChangesToIDuringYSuffixation()) {
                 State transitionState = new State("VerbalStem", false, false);
                 State startState = new State("VerbalRoot", true, false);
-                Transition transition = new Transition(transitionState,"Hyor", "PROG1");
+                Transition transition = new Transition(transitionState, "Hyor", "PROG1");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -91,18 +91,18 @@ public class FsmMorphologicalAnalyzerTest {
     }
 
     @Test
-    public void morphologicalAnalysisIsPortmanteau(){
+    public void morphologicalAnalysisIsPortmanteau() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.isPortmanteau() && !word.isPlural() && !word.isPortmanteauFacedVowelEllipsis()){
+            if (word.isNominal() && word.isPortmanteau() && !word.isPlural() && !word.isPortmanteauFacedVowelEllipsis()) {
                 State transitionState = new State("CompoundNounRoot", true, false);
                 State startState = new State("CompoundNounRoot", true, false);
-                Transition transition = new Transition(transitionState,"lArH", "A3PL+P3PL");
+                Transition transition = new Transition(transitionState, "lArH", "A3PL+P3PL");
                 String rootForm, surfaceForm, exceptLast2, exceptLast;
                 exceptLast2 = word.getName().substring(0, word.getName().length() - 2);
                 exceptLast = word.getName().substring(0, word.getName().length() - 1);
-                if (word.isPortmanteauFacedSoftening()){
+                if (word.isPortmanteauFacedSoftening()) {
                     switch (word.getName().charAt(word.getName().length() - 2)) {
                         case 'b':
                             rootForm = exceptLast2 + 'p';
@@ -120,7 +120,7 @@ public class FsmMorphologicalAnalyzerTest {
                             rootForm = exceptLast;
                     }
                 } else {
-                    if (word.isPortmanteauEndingWithSI()){
+                    if (word.isPortmanteauEndingWithSI()) {
                         rootForm = exceptLast2;
                     } else {
                         rootForm = exceptLast;
@@ -135,12 +135,12 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisNotObeysVowelHarmonyDuringAgglutination() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.notObeysVowelHarmonyDuringAgglutination()){
+            if (word.isNominal() && word.notObeysVowelHarmonyDuringAgglutination()) {
                 State transitionState = new State("Possessive", false, false);
                 State startState = new State("NominalRoot", true, false);
-                Transition transition = new Transition(transitionState,"yH", "ACC");
+                Transition transition = new Transition(transitionState, "yH", "ACC");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -150,12 +150,12 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisLastIdropsDuringSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.lastIdropsDuringSuffixation()){
+            if (word.isNominal() && word.lastIdropsDuringSuffixation()) {
                 State transitionState = new State("Possessive", false, false);
                 State startState = new State("NominalRoot", true, false);
-                Transition transition = new Transition(transitionState,"yH", "ACC");
+                Transition transition = new Transition(transitionState, "yH", "ACC");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -163,14 +163,14 @@ public class FsmMorphologicalAnalyzerTest {
     }
 
     @Test
-    public void morphologicalAnalysisVerbSoftenDuringSuffixation(){
+    public void morphologicalAnalysisVerbSoftenDuringSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isVerb() && word.verbSoftenDuringSuffixation()){
+            if (word.isVerb() && word.verbSoftenDuringSuffixation()) {
                 State transitionState = new State("VerbalStem", false, false);
                 State startState = new State("VerbalRoot", true, false);
-                Transition transition = new Transition(transitionState,"Hyor", "PROG1");
+                Transition transition = new Transition(transitionState, "Hyor", "PROG1");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -180,12 +180,12 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisDuplicatesDuringSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.duplicatesDuringSuffixation()){
+            if (word.isNominal() && word.duplicatesDuringSuffixation()) {
                 State transitionState = new State("Possessive", false, false);
                 State startState = new State("NominalRoot", true, false);
-                Transition transition = new Transition(transitionState,"yH", "ACC");
+                Transition transition = new Transition(transitionState, "yH", "ACC");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -195,12 +195,12 @@ public class FsmMorphologicalAnalyzerTest {
     @Test
     public void morphologicalAnalysisEndingKChangesIntoG() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isNominal() && word.endingKChangesIntoG()){
+            if (word.isNominal() && word.endingKChangesIntoG()) {
                 State transitionState = new State("Possessive", false, false);
                 State startState = new State("NominalRoot", true, false);
-                Transition transition = new Transition(transitionState,"yH", "ACC");
+                Transition transition = new Transition(transitionState, "yH", "ACC");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
@@ -208,19 +208,20 @@ public class FsmMorphologicalAnalyzerTest {
     }
 
     @Test
-    public void morphologicalAnalysisLastIdropsDuringPassiveSuffixation(){
+    public void morphologicalAnalysisLastIdropsDuringPassiveSuffixation() {
         TxtDictionary dictionary = fsm.getDictionary();
-        for (int i = 0; i < dictionary.size(); i++){
+        for (int i = 0; i < dictionary.size(); i++) {
             TxtWord word = (TxtWord) dictionary.getWord(i);
-            if (word.isVerb() && word.lastIdropsDuringPassiveSuffixation()){
+            if (word.isVerb() && word.lastIdropsDuringPassiveSuffixation()) {
                 State transitionState = new State("VerbalStem", false, false);
                 State startState = new State("VerbalRoot", true, false);
-                Transition transition = new Transition(transitionState,"Hl", "^DB+VERB+PASS");
+                Transition transition = new Transition(transitionState, "Hl", "^DB+VERB+PASS");
                 String surfaceForm = transition.makeTransition(word, word.getName(), startState);
                 assertTrue(fsm.morphologicalAnalysis(surfaceForm).size() != 0);
             }
         }
     }
+
     @Test
     public void testReplaceWord() {
         assertEquals("Şvesterine söyle kazağı güzelmiş", fsm.replaceWord(new Sentence("Hemşirene söyle kazağı güzelmiş"), "hemşire", "şvester").toString());
@@ -238,34 +239,25 @@ public class FsmMorphologicalAnalyzerTest {
     }
 
     @Test
-    public void testGenerateAllParses() {
+    public void testGenerateAllParses() throws IOException {
         String[] testWords = {"açıkla", "yıldönümü", "resim", "hal", "cenk", "emlak", "git", "kalp", "kavur", "ye", "yemek", "göç", "ak", "sıska", "yıka", "bul",
                 "cevapla", "coş", "böl", "del", "giy", "kaydol", "anla", "çök", "çık", "doldur", "azal", "göster", "aksa"};
-        ArrayList<TxtWord> words = new ArrayList<>();
         ArrayList<FsmParse> parsesGenerated;
         BufferedReader reader;
         String line;
-
         for (int i = 1; i < testWords.length; i++) {
-            words.add((TxtWord)fsm.getDictionary().getWord(testWords[i]));
-        }
-        try {
-            for (TxtWord word : words) {
-                ArrayList<String> parsesExpected = new ArrayList<>();
-                reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("\\parses\\" + word.getName() + ".txt")), StandardCharsets.UTF_8));
-                while ((line = reader.readLine()) != null) {
-                    parsesExpected.add(line.split(" ")[1]);
-                }
-                reader.close();
-                parsesGenerated = fsm.generateAllParses(word, word.getName().length() + 5);
-                assertEquals("\"" + word.getName() + ".txt\"", parsesExpected.size(), parsesGenerated.size());
-                for (FsmParse parseGenerated : parsesGenerated) {
-                    assertTrue( "\"" + word.getName() + ".txt\"", parsesExpected.contains(parseGenerated.toString()));
-                }
+            TxtWord word = (TxtWord) fsm.getDictionary().getWord(testWords[i]);
+            ArrayList<String> parsesExpected = new ArrayList<>();
+            reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("parses/" + word.getName() + ".txt")), StandardCharsets.UTF_8));
+            while ((line = reader.readLine()) != null) {
+                parsesExpected.add(line.split(" ")[1]);
             }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
+            reader.close();
+            parsesGenerated = fsm.generateAllParses(word, word.getName().length() + 5);
+            assertEquals("\"" + word.getName() + ".txt\"", parsesExpected.size(), parsesGenerated.size());
+            for (FsmParse parseGenerated : parsesGenerated) {
+                assertTrue("\"" + word.getName() + ".txt\"", parsesExpected.contains(parseGenerated.toString()));
+            }
         }
     }
 }
